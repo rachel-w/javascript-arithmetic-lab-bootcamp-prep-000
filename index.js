138c2bd6267a320597cfa1388cf96b1f5cac1277
Rachel Wey <rachelwey96@outlook.com>
2018-10-23 08:22:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-var a, b
+var a, b, n
 
 function add(){
   return (a + b)
 }
 
 function subtract(){
-
+  return (a - b)
 }
 
 function multiply(){
-
+  return (a * b)
 }
 
 function divide(){
-
+  return (a / b)
 }
 
 function inc(n){
-
+  return (a += b)
 }
 
 function dec(n){
-
+  return (a -= b)
 }
 
 function makeInt(n){
